0daba17c724cbaa3193cd087a519cbb6cb1a32a9
Criomby <86114549+Criomby@users.noreply.github.com>
2021-06-23 10:50:59 +0200

    Update to v2.1.1
    Feature 'Leads by industries' implemented into pandas

--- functions.py
+++ functions.py
@@ -29,41 +29,57 @@ def get_topleads(inputfile):
 def printall_org_table(inputfile):
     global table_savestate1
     hubspot_org = pd.read_csv(inputfile)
-    csv1 = hubspot_org[['Name', 'Lead Status', 'Industry', 'Create Date']] # sort date descending? -> usually not necessary bc. already done by HubSpot on export
+    csv1 = hubspot_org[['Name', 'Lead Status', 'Industry', 'Create Date']] # sort date descending? -> usually not necessary bc. already done by HubSpot at export
     table_savestate1 = csv1
     return csv1
 
-# // not yet translated into pandas //
-"""
-def leadsbyindustry(inputfile): #create 'for' loop to avoid redundancy
+def leadsbyindustry(inputfile): #create 'for' loop to make function smaller
     global table_savestate1
     hubspot_org = pd.read_csv(inputfile)
     csv1 = hubspot_org[['Lead Status', 'Industry']]
-    basetable = pd.DataFrame(data = {'Lead Status': [''], 'count': ['']})
-    insurance = csv1[csv1['Industry'] == 'Insurance'].groupby('Lead Status').size()
-    finance = csv1[csv1['Industry'] == 'Financial Services'].groupby('Lead Status').size()
-    mobility = csv1[csv1['Industry'] == 'Mobility'].groupby('Lead Status').size()
-    health = csv1[csv1['Industry'] == 'Health'].groupby('Lead Status').size()
-    telecomm = csv1[csv1['Industry'] == 'Telecommunications'].groupby('Lead Status').size()
-    ecommerce = csv1[csv1['Industry'] == 'eCommerce'].groupby('Lead Status').size()
-    broker = csv1[csv1['Industry'] == 'Online Broker'].groupby('Lead Status').size()
-    edu = csv1[csv1['Industry'] == 'Education'].groupby('Lead Status').size()
-    banking = csv1[csv1['Industry'] == 'Banking'].groupby('Lead Status').size()
-    government = csv1[csv1['Industry'] == 'Government'].groupby('Lead Status').size()
-    nan = csv1[csv1['Industry'] == 'nan'].groupby('Lead Status').size()
-    #total = basetable.append(insurance)
-    total = basetable.with_row(make_array('', '')).with_row(make_array('Insurance', '')).append(insurance).with_row(
-        make_array('', '')).with_row(make_array('Financial Services', '')).append(finance).with_row(
-        make_array('', '')).with_row(make_array('Mobilty', '')).append(mobility).with_row(make_array('', '')).with_row(
-        make_array('Health', '')).append(health).with_row(make_array('', '')).with_row(
-        make_array('Telecommunications', '')).append(telecomm).with_row(make_array('', '')).with_row(
-        make_array('eCommerce', '')).append(ecommerce).with_row(make_array('', '')).with_row(
-        make_array('Broker', '')).append(broker).with_row(make_array('', '')).with_row(
-        make_array('Education', '')).append(edu).with_row(make_array('', '')).with_row(
-        make_array('Banking', '')).append(banking).with_row(make_array('', '')).with_row(
-        make_array('Other', '')).append(nan)
+    """
+    # test for function and get it right (decreased code size (speed?))
+    CATEGORIES = ['Insurance', 'Financial Services', 'Mobility', 'Health', 'Telecommunications', 'eCommerce',
+                  'Online Broker', 'Education',
+                  'Banking', 'Government', 'nan']
+    VARIABLES = [insurance, finance, mobility, health, telecomm, ecommerce, broker, edu, banking, government, nan]
+    for i in VARIABLES:
+        i = csv1[csv1['Industry'] == i].groupby('Lead Status').size().to_frame()
+        df1 = pd.DataFrame([['i', '']], columns=i.columns)
+        total = df1.append(finance).append(df1).append(insurance).append(df1).append(mobility).append(df1).append(
+            health).append(df1).append(telecomm).append(df1).append(ecommerce).append(
+            df1).append(broker).append(df1).append(edu).append(df1).append(banking).append(df1).append(government).append(
+            df1).append(nan)
+    """
+    insurance = csv1[csv1['Industry'] == 'Insurance'].groupby('Lead Status').size().to_frame().reset_index()
+    finance = csv1[csv1['Industry'] == 'Financial Services'].groupby('Lead Status').size().to_frame().reset_index()
+    mobility = csv1[csv1['Industry'] == 'Mobility'].groupby('Lead Status').size().to_frame().reset_index()
+    health = csv1[csv1['Industry'] == 'Health'].groupby('Lead Status').size().to_frame().reset_index()
+    telecomm = csv1[csv1['Industry'] == 'Telecommunications'].groupby('Lead Status').size().to_frame().reset_index()
+    ecommerce = csv1[csv1['Industry'] == 'eCommerce'].groupby('Lead Status').size().to_frame().reset_index()
+    broker = csv1[csv1['Industry'] == 'Online Broker'].groupby('Lead Status').size().to_frame().reset_index()
+    edu = csv1[csv1['Industry'] == 'Education'].groupby('Lead Status').size().to_frame().reset_index()
+    #banking = csv1[csv1['Industry'] == 'Banking'].groupby('Lead Status').size().to_frame().reset_index() # depreciated
+    government = csv1[csv1['Industry'] == 'Government'].groupby('Lead Status').size().to_frame().reset_index()
+    nan = csv1[csv1['Industry'] == 'nan'].groupby('Lead Status').size().to_frame().reset_index()
+    df0 = pd.DataFrame([['', '']], columns=insurance.columns)
+    df1 = pd.DataFrame([['Insurance', '']], columns=insurance.columns) # add 'for' function for each category in 'Industries' respectively
+    df2 = pd.DataFrame([['Financial Services', '']], columns=insurance.columns)
+    df3 = pd.DataFrame([['Mobility', '']], columns=insurance.columns)
+    df4 = pd.DataFrame([['Health', '']], columns=insurance.columns)
+    df5 = pd.DataFrame([['Telecommunications', '']], columns=insurance.columns)
+    df6 = pd.DataFrame([['eCommerce', '']], columns=insurance.columns)
+    df7 = pd.DataFrame([['Online Broker', '']], columns=insurance.columns)
+    df8 = pd.DataFrame([['Education', '']], columns=insurance.columns)
+    #df8 = pd.DataFrame([['Banking', '']], columns=insurance.columns) # depreciated
+    df9 = pd.DataFrame([['Government', '']], columns=insurance.columns)
+    df10 = pd.DataFrame([['NaN', '']], columns=insurance.columns)
+    total = df1.append(insurance).append(df0).append(df2).append(finance).append(df0).append(df3).append(mobility).append(
+        df0).append(df4).append(health).append(df0).append(df5).append(telecomm).append(df0).append(df6).append(
+        ecommerce).append(df0).append(df7).append(broker).append(df0).append(df8).append(edu).append(df0).append(
+        df9).append(government).append(df0).append(df10).append(nan).rename(columns={0: 'Count'})
     table_savestate1 = total
-    return insurance #total"""
+    return total
 
 # // not yet translated into pandas //
 """def join_contacts(companies, contacts):
