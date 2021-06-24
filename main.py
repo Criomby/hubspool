a77071639afad25daa118f560530554d5232b28b
Criomby <86114549+Criomby@users.noreply.github.com>
2021-06-24 09:29:21 +0200

    Update to v2.2.0
    First production release!

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import numpy as np
 import pandas as pd
 from functions import *
 
+pd.set_option('display.max_rows', 20)
+pd.set_option('display.max_columns', 4)
+#pd.set_option('display.width', 100)
+
 # global variables for data storage across functions
 table_savestate1 = pd.DataFrame()
 
@@ -100,17 +104,27 @@ def start_leadsbyindustry():
         textbox.insert('1.0', '--------------------------------------------------------------------------------')
         textbox.insert('1.0', '\n')
 
+def start_pitches():
+    global table_savestate1
+    x = entry_filepath_open.get()
+    if os.path.isfile(x):
+        result = pitches(x)
+        table_savestate1 = result
+        textbox.insert('1.0', result)
+        textbox.insert('1.0', '\n')
+        textbox.insert('1.0', '--------------------------------------------------------------------------------')
+        textbox.insert('1.0', '\n')
+    else:
+        textbox.insert('1.0', 'File not found. Check file path.')
+        textbox.insert('1.0', '\n')
+        textbox.insert('1.0', '--------------------------------------------------------------------------------')
+        textbox.insert('1.0', '\n')
+
+# 'join' feature, feature has been suspended until further notice
+"""
 def start_join():
     x = entry_filepath_open.get()
     y = entry_filepath_open_cont.get()
-
-    # placeholder until function is translated into pandas
-    textbox.insert('1.0', '** Feature in development. **')
-    textbox.insert('1.0', '\n')
-    textbox.insert('1.0', '--------------------------------------------------------------------------------')
-    textbox.insert('1.0', '\n')
-
-"""
     if os.path.isfile(entry_filepath_open.get()):
         result = join_contacts(x,y)
         textbox.insert('1.0', result)
@@ -126,7 +140,7 @@ def start_join():
 
 def check_files():
     x = entry_filepath_open.get()
-    y = entry_filepath_open_cont.get()
+    #y = entry_filepath_open_cont.get()
     s = entry_filepath_save.get()
     textbox.delete('1.0', tk.END)
     if os.path.isfile(s) == True:
@@ -139,7 +153,7 @@ def check_files():
         textbox.insert('1.0', '\n')
         textbox.insert('1.0', '--------------------------------------------------------------------------------')
         textbox.insert('1.0', '\n')
-    if os.path.isfile(y) == True:
+    """if os.path.isfile(y) == True: #function for old contacts.csv
         textbox.insert('1.0', 'Contacts file found!')
         textbox.insert('1.0', '\n')
         textbox.insert('1.0', '--------------------------------------------------------------------------------')
@@ -148,7 +162,7 @@ def check_files():
         textbox.insert('1.0', 'Contacts file NOT found!')
         textbox.insert('1.0', '\n')
         textbox.insert('1.0', '--------------------------------------------------------------------------------')
-        textbox.insert('1.0', '\n')
+        textbox.insert('1.0', '\n')"""
     if os.path.isfile(x) == True:
         textbox.insert('1.0', 'Companies file found!')
         textbox.insert('1.0', '\n')
@@ -170,7 +184,7 @@ window.resizable(False, True)
 # DEFINITIONS
 # basic layout
 frame_0 = tk.Frame()
-frame_0.pack(side='top', anchor='nw')
+frame_0.pack()
 frame_a = tk.Frame()
 frame_a.pack()
 frame_buttons1 = tk.Frame()
@@ -183,26 +197,27 @@ frame_d = tk.Frame()
 frame_d.pack()
 # define labels
 label_open_comp = tk.Label(text='Open COMPANIES.csv file from:', width=30, height=1, master=frame_a)
-label_open_cont = tk.Label(text='Open CONTACTS.csv file from:', width=30, height=1, master=frame_a)
+#label_open_cont = tk.Label(text='Open CONTACTS.csv file from:', width=30, height=1, master=frame_a)
 label_save = tk.Label(text='Safe .csv file to:', width=30, height=1, master=frame_a)
-#label_space_top = tk.Label(text='', width=30, height=1, master=frame_a)
+label_space_top = tk.Label(text='Hubspool', width=30, height=1, master=frame_a)
+label_space_top.config(font=("Courier", 44))
 label_space = tk.Label(text='', width=30, height=2, master=frame_a)
 label_space2 = tk.Label(text='', width=30, height=1, master=frame_c)
 label_space3 = tk.Label(text='', width=30, height=0, master=frame_a)
-label_space4 = tk.Label(text='', width=30, height=0, master=frame_a)
+#label_space4 = tk.Label(text='', width=30, height=0, master=frame_a)
 #label3 = tk.Label(text='Output:', width=30, height=2, master=frame_c)
 label4 = tk.Label(text='Copyright 2021 Braum                                                                            ',
                   width=50, height=1, master=frame_0)
-label5 = tk.Label(text='                                                                            Version: 2.1.2',
+label5 = tk.Label(text='                                                                            Version: 2.2.0',
                   width=50, height=1, master=frame_0)
 #label_empt = tk.Label(text='', width=40, height=1, master=frame_0)
 # define entries
 entry_filepath_open = tk.Entry(width=80, master=frame_a)
-entry_filepath_open.insert(0, 'C:/Users/name/Downloads/data.csv')
-entry_filepath_open_cont = tk.Entry(width=80, master=frame_a)
-entry_filepath_open_cont.insert(0, 'C:/Users/name/Downloads/data2.csv')
+entry_filepath_open.insert(0, 'C:/Users/NAME/Downloads/DATA.csv')
+#entry_filepath_open_cont = tk.Entry(width=80, master=frame_a)
+#entry_filepath_open_cont.insert(0, 'C:/Users/NAME/Downloads/CONTACTS_DATA.csv')
 entry_filepath_save = tk.Entry(width=80, master=frame_a)
-entry_filepath_save.insert(0, 'C:/Users/name/Downloads/output.csv')
+entry_filepath_save.insert(0, 'C:/Users/NAME/Downloads/OUTPUT.csv')
 # define textbox
 textbox = tk.Text(width=100, height=200, master=frame_d)
 # define buttons
@@ -217,7 +232,7 @@ button_printall = tk.Button(text='Print all', width=15, height=2, bg='gray85', c
                             master=frame_buttons1)
 button_leadsbyindustry = tk.Button(text='Leads by industry', width=15, height=2, bg='gray85',
                                    command=start_leadsbyindustry, master=frame_buttons1)
-button_join = tk.Button(text='Join cont. & comp.', width=15, height=2, command=start_join, bg='gray85',
+button_pitches = tk.Button(text='Pitches', width=15, height=2, command=start_pitches, bg='gray85',
                         master=frame_buttons1)
 button_check = tk.Button(text='Check files', width=15, height=2, bg='alice blue', command=check_files,
                          master=frame_buttons1)
@@ -226,23 +241,23 @@ button_check = tk.Button(text='Check files', width=15, height=2, bg='alice blue'
 label4.pack(side=tk.LEFT)
 label5.pack(side=tk.RIGHT)
 #label_empt.pack(side=tk.RIGHT)
-#label_space_top.pack()
+label_space_top.pack(pady='20')
 label_open_comp.pack()
 entry_filepath_open.pack()
-label_space4.pack()
-label_open_cont.pack()
-entry_filepath_open_cont.pack()
+#label_space4.pack()
+#label_open_cont.pack()
+#entry_filepath_open_cont.pack()
 label_space3.pack()
 label_save.pack()
 entry_filepath_save.pack()
 label_space.pack()
 button_printall.grid(row=1, column=0)
 button_counts.grid(row=0, column=1)
-# button_open.grid(row=0, column=1)
+#button_open.grid(row=0, column=1)
 button_topleads.grid(row=1, column=1)
 button_safe.grid(row=1, column=3)
 button_leadsbyindustry.grid(row=0, column=2)
-button_join.grid(row=1, column=2)
+button_pitches.grid(row=1, column=2)
 button_check.grid(row=0, column=0)
 
 button_delete.grid(row=0, column=3)
