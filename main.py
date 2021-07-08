257d7a34081541000c26442144b4a690c250e59b
Criomby <86114549+Criomby@users.noreply.github.com>
2021-07-08 16:08:29 +0200

    Update to v2.4.0

--- main.py
+++ main.py
@@ -29,27 +29,31 @@ def start_counts():
         df2 = pd.DataFrame([['Industry', '']], columns=df0.columns)
         total = df1.append(leads_inorder).append(df0).append(df2).append(cats_inorder)
         table_savestate1 = total
-        textbox.insert('1.0', leads_disordered)
-        textbox.insert('1.0', '\n')
-        textbox.insert('1.0', 'Lead categories sorted by size:')
-        textbox.insert('1.0', '\n')
         textbox.insert('1.0', '--------------------------------------------------------------------------------')
-        textbox.insert('1.0', '\n')
-        textbox.insert('1.0', cats_disordered)
-        textbox.insert('1.0', '\n')
-        textbox.insert('1.0', 'Industries sorted by size:')
-        textbox.insert('1.0', '\n')
-        textbox.insert('1.0', '--------------------------------------------------------------------------------')
-        textbox.insert('1.0', '\n')
-        textbox.insert('1.0', total)
-        textbox.insert('1.0', '\n')
-        textbox.insert('1.0', '--------------------------------------------------------------------------------')
-        textbox.insert('1.0', '\n')
+        textbox.insert('end', '\n')
+        textbox.insert('end', 'Lead categories sorted by size:')
+        textbox.insert('end', '\n')
+        textbox.insert('end', leads_disordered)
+        textbox.insert('end', '\n')
+        textbox.insert('end', '--------------------------------------------------------------------------------')
+        textbox.insert('end', '\n')
+        textbox.insert('end', 'Industries sorted by size:')
+        textbox.insert('end', '\n')
+        textbox.insert('end', cats_disordered)
+        textbox.insert('end', '\n')
+        textbox.insert('end', '--------------------------------------------------------------------------------')
+        textbox.insert('end', '\n')
+        textbox.insert('end', total)
+        textbox.insert('end', '\n')
+        textbox.insert('end', '--------------------------------------------------------------------------------')
+        textbox.insert('end', '\n')
     else:
-        textbox.insert('1.0', 'No file opened. Select a file first.')
-        textbox.insert('1.0', '\n')
         textbox.insert('1.0', '--------------------------------------------------------------------------------')
-        textbox.insert('1.0', '\n')
+        textbox.insert('end', '\n')
+        textbox.insert('end', 'Error: No file opened. Select a file first.')
+        textbox.insert('end', '\n')
+        textbox.insert('end', '--------------------------------------------------------------------------------')
+        textbox.insert('end', '\n')
 
 def start_topleads():
     global table_savestate1
@@ -58,16 +62,19 @@ def start_topleads():
     if os.path.isfile(company_data_filepath):
         result = get_topleads(company_data_filepath)
         table_savestate1 = result
-        # textbox.insert('1.0', 'Our top leads are:')
-        textbox.insert('1.0', result)
-        textbox.insert('1.0', '\n')
         textbox.insert('1.0', '--------------------------------------------------------------------------------')
-        textbox.insert('1.0', '\n')
+        textbox.insert('end', '\n')
+        textbox.insert('end', result)
+        textbox.insert('end', '\n')
+        textbox.insert('end', '--------------------------------------------------------------------------------')
+        textbox.insert('end', '\n')
     else:
-        textbox.insert('1.0', 'No file opened. Select a file first.')
-        textbox.insert('1.0', '\n')
         textbox.insert('1.0', '--------------------------------------------------------------------------------')
-        textbox.insert('1.0', '\n')
+        textbox.insert('end', '\n')
+        textbox.insert('end', 'Error: No file opened. Select a file first.')
+        textbox.insert('end', '\n')
+        textbox.insert('end', '--------------------------------------------------------------------------------')
+        textbox.insert('end', '\n')
 
 def printall_button():
     global company_data_filepath
@@ -76,15 +83,19 @@ def printall_button():
     textbox.delete('1.0', tk.END)
     if os.path.isfile(x):
         result = printall_org_table(x)
-        textbox.insert('1.0', result)
-        textbox.insert('1.0', '\n')
         textbox.insert('1.0', '--------------------------------------------------------------------------------')
-        textbox.insert('1.0', '\n')
+        textbox.insert('end', '\n')
+        textbox.insert('end', result)
+        textbox.insert('end', '\n')
+        textbox.insert('end', '--------------------------------------------------------------------------------')
+        textbox.insert('end', '\n')
     else:
-        textbox.insert('1.0', 'No file opened. Select a file first.')
-        textbox.insert('1.0', '\n')
         textbox.insert('1.0', '--------------------------------------------------------------------------------')
-        textbox.insert('1.0', '\n')
+        textbox.insert('end', '\n')
+        textbox.insert('end', 'Error: No file opened. Select a file first.')
+        textbox.insert('end', '\n')
+        textbox.insert('end', '--------------------------------------------------------------------------------')
+        textbox.insert('end', '\n')
 
 def start_leadsbyindustry():
     global table_savestate1
@@ -94,15 +105,19 @@ def start_leadsbyindustry():
     if os.path.isfile(x):
         result = leadsbyindustry(x)
         table_savestate1 = result
-        textbox.insert('1.0', result)
-        textbox.insert('1.0', '\n')
         textbox.insert('1.0', '--------------------------------------------------------------------------------')
-        textbox.insert('1.0', '\n')
+        textbox.insert('end', '\n')
+        textbox.insert('end', result)
+        textbox.insert('end', '\n')
+        textbox.insert('end', '--------------------------------------------------------------------------------')
+        textbox.insert('end', '\n')
     else:
-        textbox.insert('1.0', 'No file opened. Select a file first.')
-        textbox.insert('1.0', '\n')
         textbox.insert('1.0', '--------------------------------------------------------------------------------')
-        textbox.insert('1.0', '\n')
+        textbox.insert('end', '\n')
+        textbox.insert('end', 'Error: No file opened. Select a file first.')
+        textbox.insert('end', '\n')
+        textbox.insert('end', '--------------------------------------------------------------------------------')
+        textbox.insert('end', '\n')
 
 def start_pitches():
     global table_savestate1
@@ -112,15 +127,19 @@ def start_pitches():
     if os.path.isfile(x):
         result = pitches(x)
         table_savestate1 = result
-        textbox.insert('1.0', result)
-        textbox.insert('1.0', '\n')
         textbox.insert('1.0', '--------------------------------------------------------------------------------')
-        textbox.insert('1.0', '\n')
+        textbox.insert('end', '\n')
+        textbox.insert('end', result)
+        textbox.insert('end', '\n')
+        textbox.insert('end', '--------------------------------------------------------------------------------')
+        textbox.insert('end', '\n')
     else:
-        textbox.insert('1.0', 'No file opened. Select a file first.')
-        textbox.insert('1.0', '\n')
         textbox.insert('1.0', '--------------------------------------------------------------------------------')
-        textbox.insert('1.0', '\n')
+        textbox.insert('end', '\n')
+        textbox.insert('end', 'Error: No file opened. Select a file first.')
+        textbox.insert('end', '\n')
+        textbox.insert('end', '--------------------------------------------------------------------------------')
+        textbox.insert('end', '\n')
 
 def select_file():
     global company_data_filepath
@@ -129,21 +148,27 @@ def select_file():
         ('All files', '*.*')
     )
     filename = fd.askopenfilename(
-        title='Open a file',
-        initialdir='/',
-        filetypes=filetypes)
+    title='Open a file',
+    initialdir='/',
+    filetypes=filetypes)
     company_data_filepath = filename
     textbox.delete('1.0', tk.END)
     if filename == '':
-        textbox.insert('1.0', 'No file opened.')
-        textbox.insert('1.0', '\n')
         textbox.insert('1.0', '--------------------------------------------------------------------------------')
-        textbox.insert('1.0', '\n')
+        textbox.insert('end', '\n')
+        textbox.insert('end', 'No file selected.')
+        textbox.insert('end', '\n')
+        textbox.insert('end', '--------------------------------------------------------------------------------')
+        textbox.insert('end', '\n')
     else:
-        textbox.insert('1.0', 'Opened file: ' + filename)
-        textbox.insert('1.0', '\n')
         textbox.insert('1.0', '--------------------------------------------------------------------------------')
-        textbox.insert('1.0', '\n')
+        textbox.insert('end', '\n')
+        textbox.insert('end', 'Opened file:')
+        textbox.insert('end', '\n')
+        textbox.insert('end', filename)
+        textbox.insert('end', '\n')
+        textbox.insert('end', '--------------------------------------------------------------------------------')
+        textbox.insert('end', '\n')
 
 def save_file():
     global table_savestate1
@@ -152,33 +177,54 @@ def save_file():
         ('csv files', '*.csv'),
         ('All files', '*.*')
     )
-    filename = fd.asksaveasfilename(
-        title='Save a file',
-        initialdir='/',
-        filetypes=filetypes)
-    #textbox.delete('1.0', tk.END)
+    textbox.delete('1.0', tk.END)
     if table_savestate1.empty == True:
-        textbox.insert('1.0', 'Execute a function first to save it.')
-        textbox.insert('1.0', '\n')
         textbox.insert('1.0', '--------------------------------------------------------------------------------')
-        textbox.insert('1.0', '\n')
-    elif filename == '':
-        textbox.insert('1.0', 'No file saved.')
-        textbox.insert('1.0', '\n')
-        textbox.insert('1.0', '--------------------------------------------------------------------------------')
-        textbox.insert('1.0', '\n')
+        textbox.insert('end', '\n')
+        textbox.insert('end', 'Error: Execute a function first to save it.')
+        textbox.insert('end', '\n')
+        textbox.insert('end', '--------------------------------------------------------------------------------')
+        textbox.insert('end', '\n')
     else:
-        table_savestate1.to_excel(filename + '.xlsx')
-        textbox.insert('1.0', 'Saved file: ' + filename)
-        textbox.insert('1.0', '\n')
-        textbox.insert('1.0', '--------------------------------------------------------------------------------')
-        textbox.insert('1.0', '\n')
+        filename = fd.asksaveasfilename(
+            title='Save a file',
+            initialdir='/',
+            filetypes=filetypes)
+        if filename == '':
+            textbox.insert('1.0', '--------------------------------------------------------------------------------')
+            textbox.insert('end', '\n')
+            textbox.insert('end', 'No file saved.')
+            textbox.insert('end', '\n')
+            textbox.insert('end', '--------------------------------------------------------------------------------')
+            textbox.insert('end', '\n')
+        else:
+            table_savestate1.to_excel(filename + '.xlsx')
+            textbox.insert('1.0', '--------------------------------------------------------------------------------')
+            textbox.insert('end', '\n')
+            textbox.insert('end', 'Saved file: ' + filename)
+            textbox.insert('end', '\n')
+            textbox.insert('end', '--------------------------------------------------------------------------------')
+            textbox.insert('end', '\n')
 
 def exex_all():
     global company_data_filepath
     x = company_data_filepath
+    new_filepath = x[:-4] + '_Hubspool.xlsx'
     textbox.delete('1.0', tk.END)
-    if os.path.isfile(x):
+    if os.path.isfile(new_filepath) == True:
+        textbox.insert('1.0', '--------------------------------------------------------------------------------')
+        textbox.insert('end', '\n')
+        textbox.insert('end', 'Error: File already exists. ')
+        textbox.insert('end', '\n')
+        textbox.insert('end', 'Open another file for analysis or delete existing file.')
+        textbox.insert('end', '\n')
+        textbox.insert('end', 'Existing file:')
+        textbox.insert('end', '\n')
+        textbox.insert('end', new_filepath)
+        textbox.insert('end', '\n')
+        textbox.insert('end', '--------------------------------------------------------------------------------')
+        textbox.insert('end', '\n')
+    elif os.path.isfile(x) == True:
         # counts
         leads_disordered, leads_inorder = lead_count(x)
         cats_disordered, cats_inorder = industry_count(x)
@@ -189,41 +235,47 @@ def exex_all():
         table_savestate1 = total
         table_savestate2 = leads_disordered
         table_savestate3 = cats_disordered
-        textbox.insert('1.0', 'Topleads done.')
-        textbox.insert('1.0', '\n')
+        textbox.insert('1.0', '--------------------------------------------------------------------------------')
+        textbox.insert('end', '\n')
+        textbox.insert('end', 'Counts (ordered) done.')
+        textbox.insert('end', '\n')
+        textbox.insert('end', 'Counts (raw / size sorted) done.')
+        textbox.insert('end', '\n')
         # leads by industry
         table_savestate4 = leadsbyindustry(x)
-        textbox.insert('1.0', 'Pitches done.')
-        textbox.insert('1.0', '\n')
+        textbox.insert('end', 'Leads by industry done.')
+        textbox.insert('end', '\n')
         # pitches
         table_savestate5 = pitches(x)
-        textbox.insert('1.0', 'Leads by industry done.')
-        textbox.insert('1.0', '\n')
+        textbox.insert('end', 'Pitches done.')
+        textbox.insert('end', '\n')
         # topleads
         table_savestate6 = get_topleads(x)
-        textbox.insert('1.0', 'Counts (ordered & raw) done.')
-        textbox.insert('1.0', '\n')
-        textbox.insert('1.0', '--------------------------------------------------------------------------------')
-        textbox.insert('1.0', '\n')
-        with pd.ExcelWriter(x + '_Hubspool.xlsx') as writer:
+        textbox.insert('end', 'Topleads done.')
+        textbox.insert('end', '\n')
+        textbox.insert('end', '--------------------------------------------------------------------------------')
+        textbox.insert('end', '\n')
+        with pd.ExcelWriter(new_filepath) as writer:
             table_savestate1.to_excel(writer, sheet_name='Counts+Industries')
             table_savestate2.to_excel(writer, sheet_name='Counts_raw')
             table_savestate3.to_excel(writer, sheet_name='Industries_raw')
             table_savestate4.to_excel(writer, sheet_name='Leads_by_industries')
             table_savestate5.to_excel(writer, sheet_name='Pitches')
             table_savestate6.to_excel(writer, sheet_name='Topleads')
-        textbox.insert('1.0', '\n')
-        textbox.insert('1.0', x + 'Hubspool')
-        textbox.insert('1.0', '\n')
-        textbox.insert('1.0', 'Excel file generated and saved to:')
-        textbox.insert('1.0', '\n')
-        textbox.insert('1.0', '--------------------------------------------------------------------------------')
-        textbox.insert('1.0', '\n')
+        textbox.insert('end', '\n')
+        textbox.insert('end', 'Excel file generated and saved to:')
+        textbox.insert('end', '\n')
+        textbox.insert('end', new_filepath[:-5])
+        textbox.insert('end', '\n')
+        textbox.insert('end', '--------------------------------------------------------------------------------')
+        textbox.insert('end', '\n')
     else:
-        textbox.insert('1.0', 'Open a file first.')
-        textbox.insert('1.0', '\n')
         textbox.insert('1.0', '--------------------------------------------------------------------------------')
-        textbox.insert('1.0', '\n')
+        textbox.insert('end', '\n')
+        textbox.insert('end', 'Error: Open a file first.')
+        textbox.insert('end', '\n')
+        textbox.insert('end', '--------------------------------------------------------------------------------')
+        textbox.insert('end', '\n')
 
 # GUI
 window = tk.Tk()
@@ -249,7 +301,7 @@ label_space2 = tk.Label(text='', width=30, height=1, master=frame_c)
 label_space3 = tk.Label(text='', width=30, height=0, master=frame_a)
 label4 = tk.Label(text='Copyright 2021 Braum                                                                            ',
                   width=50, height=1, master=frame_0)
-label5 = tk.Label(text='                                                                            Version: 2.3.0',
+label5 = tk.Label(text='                                                                            Version: 2.4.0',
                   width=50, height=1, master=frame_0)
 # define textbox
 textbox = tk.Text(width=100, height=200, master=frame_d)
@@ -268,7 +320,7 @@ button_pitches = tk.Button(text='Pitches', width=15, height=2, command=start_pit
                         master=frame_buttons1)
 button_open = tk.Button(text='Open file', width=15, height=2, bg='alice blue', command=select_file,
                          master=frame_buttons1)
-button_empt0 = tk.Button(text='ALL', width=15, height=2, command=exex_all, bg='gray40', fg='white',
+button_all = tk.Button(text='ALL', width=15, height=2, command=exex_all, bg='gray40', fg='white',
                          master=frame_buttons1)
 # PACKS
 label4.pack(side=tk.LEFT)
@@ -279,12 +331,13 @@ label_space3.pack()
 button_open.grid(row=0, column=0, pady='10')
 button_safe.grid(row=0, column=1, pady='10')
 button_delete.grid(row=0, column=2, pady='10')
-button_printall.grid(row=1, column=0)
-button_counts.grid(row=1, column=1)
-button_leadsbyindustry.grid(row=1, column=2)
-button_topleads.grid(row=2, column=0)
-button_pitches.grid(row=2, column=1)
-button_empt0.grid(row=2, column=2)
+button_all.grid(row=1, column=0)
+button_printall.grid(row=1, column=1)
+button_counts.grid(row=1, column=2)
+button_leadsbyindustry.grid(row=2, column=0)
+button_topleads.grid(row=2, column=1)
+button_pitches.grid(row=2, column=2)
+
 label_space2.pack()
 textbox.pack()
 window.mainloop()
